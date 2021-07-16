7a7c30ecc6df0c60a84bd90375de4afc1cde386b
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-16 15:13:06 +0530

    Update telegraphop.py

--- MashaRoBot/modules/telegraphop.py
+++ MashaRoBot/modules/telegraphop.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-Aquila-14 = "EMCEE"
+Aquilarandi = "EMCEE"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=emcee)
 auth_url = r["auth_url"]
